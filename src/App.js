e08d8ffc8a2babab4ad309a8bd1aa02006510607
joeCodes05 <nathancodes05@gmail.com>
2023-11-11 10:31:05 -1100

    frontend mentor files setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return <div className="font-ubuntu">App</div>;
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      ubuntu: ["Ubuntu", "sans-serif"],
+    },
   },
   plugins: [],
 };
