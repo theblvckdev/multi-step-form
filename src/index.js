a58bc17f85e0396ee1e3c6a099d84d7bd3be9490
joeCodes05 <nathancodes05@gmail.com>
2023-11-13 17:05:42 +0100

    code update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/styles/index.css";
 import App from "./App";
+import { GlobalProvider } from "./context/globalContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>
 );
