346256d290c6a71059a278dec870992219e2f5f4
joeCodes05 <nathancodes05@gmail.com>
2023-11-14 09:52:01 +0100

    plan data

---
+++ src/constants/planData.js
@@ -0,0 +1,32 @@
+import AcradeIcon from '../assets/images/icon-arcade.svg'
+import AdvancedIcon from '../assets/images/icon-advanced.svg'
+import ProIcon from '../assets/images/icon-pro.svg'
+
+export const plansData = [
+    {
+        id: 1,
+        title: 'Acrade',
+        monthlyFee: 9,
+        yearlyFee: 90,
+        monthlyFreeTrial: 2,
+        iconUrl: AcradeIcon
+    },
+
+    {
+        id: 2,
+        title: 'Advanced',
+        monthlyFee: 12,
+        yearlyFee: 120,
+        monthlyFreeTrial: 2,
+        iconUrl: AdvancedIcon
+    },
+
+    {
+        id: 3,
+        title: 'Pro',
+        monthlyFee: 15,
+        yearlyFee: 150,
+        monthlyFreeTrial: 2,
+        iconUrl: ProIcon
+    }
+]
