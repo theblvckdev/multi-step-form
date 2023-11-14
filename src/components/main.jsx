9a0fe7073f8b30cb71eaab5c5086ff799b965252
joeCodes05 <nathancodes05@gmail.com>
2023-11-14 09:50:58 +0100

    code update

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -6,14 +6,39 @@ import { GlobalContex } from "../context/globalContext";
 import Button from "../utils/button";
 
 const Main = () => {
-  const { currentStep, setCompleted, setCurrentStep, completed } =
-    useContext(GlobalContex);
+  const {
+    currentStep,
+    setCompleted,
+    setCurrentStep,
+    completed,
+    name,
+    email,
+    number,
+    setValidName,
+    setValidEmail,
+    setValidNumber,
+  } = useContext(GlobalContex);
   currentStep === 1 ? setCompleted(false) : setCompleted(true);
 
   const nextStep = (e) => {
     e.preventDefault();
 
-    setCurrentStep(currentStep + 1);
+    if (name.length < 1) {
+      setValidName(false);
+    }
+     if (email.length < 1) {
+      setValidEmail(false);
+    }
+     if (number.length < 1) {
+      setValidNumber(false);
+    }
+
+    if (name.length >= 1 && email.length >= 1 && number.length >= 1) {
+      setValidName(true);
+      setValidEmail(true);
+      setValidNumber(true);
+      setCurrentStep(currentStep + 1);
+    }
   };
 
   const goBack = (e) => {
@@ -26,6 +51,7 @@ const Main = () => {
     e.preventDefault();
 
     console.log("submitted");
+    setCurrentStep(4);
   };
 
   return (
@@ -36,32 +62,34 @@ const Main = () => {
           {currentStep === 2 ? <Plans /> : null}
           {currentStep === 3 ? <AddOns /> : null}
 
-          <footer className=" absolute w-full p-3 left-0 right-0 bottom-0">
-            <div className="flex">
-              <div className="mr-auto">
-                {completed ? (
+          {currentStep === 4 ? null : (
+            <footer className=" absolute w-full p-3 left-0 right-0 bottom-0">
+              <div className="flex">
+                <div className="mr-auto">
+                  {completed ? (
+                    <Button
+                      text={"Go Back"}
+                      onClick={goBack}
+                      className={
+                        "bg-transparent text-gray-400 hover:text-primary-marineBlue"
+                      }
+                    />
+                  ) : null}
+                </div>
+                <div className="text-right">
                   <Button
-                    text={"Go Back"}
-                    onClick={goBack}
+                    text={currentStep === 3 ? "Confirm" : "Next Step"}
+                    onClick={currentStep === 3 ? submitForm : nextStep}
                     className={
-                      "bg-transparent text-gray-400 hover:text-primary-marineBlue"
+                      currentStep === 3
+                        ? "bg-primary-purplishBlue text-white"
+                        : "bg-primary-marineBlue text-white"
                     }
                   />
-                ) : null}
+                </div>
               </div>
-              <div className="text-right">
-                <Button
-                  text={currentStep === 4 ? "Confirm" : "Next Step"}
-                  onClick={currentStep === 4 ? submitForm : nextStep}
-                  className={
-                    currentStep === 4
-                      ? "bg-primary-purplishBlue text-white"
-                      : "bg-primary-marineBlue text-white"
-                  }
-                />
-              </div>
-            </div>
-          </footer>
+            </footer>
+          )}
         </form>
       </div>
     </>
