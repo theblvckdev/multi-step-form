c70d9cf00bfd8a34a924ccb41118f8276b83d30e
joeCodes05 <nathancodes05@gmail.com>
2023-11-14 16:54:42 +0100

    working on add-ons step

--- src/pages/addOns.jsx
+++ src/pages/addOns.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const AddOns = () => {
   return (
-    <div>addOns</div>
-  )
-}
+    <>
+      <div>
+        <h1 className="text-primary-marineBlue font-bold text-4xl leading-9">
+          Pick add-ons
+        </h1>
+        <h3 className="text-gray-400 mt-2">
+          Add-ons help enhance your gaming experience.
+        </h3>
+      </div>
+    </>
+  );
+};
 
-export default AddOns
+export default AddOns;
