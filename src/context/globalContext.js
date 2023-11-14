113419e95633ae1d8c3930ea0489e79c978eeddd
joeCodes05 <nathancodes05@gmail.com>
2023-11-14 09:52:10 +0100

    code update

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -5,11 +5,16 @@ export const GlobalContex = createContext();
 export const GlobalProvider = ({ children }) => {
   const [currentStep, setCurrentStep] = useState(1);
   const [completed, setCompleted] = useState(false);
-  // const 
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
+  const [validName, setValidName] = useState(true)
+  const [validEmail, setValidEmail] = useState(true)
+  const [validNumber, setValidNumber] = useState(true)
 
   return (
     <GlobalContex.Provider
-      value={{ currentStep, setCurrentStep, completed, setCompleted }}
+      value={{ currentStep, setCurrentStep, completed, setCompleted, name, setName, email, setEmail, number, setNumber, validName, setValidName, validEmail, setValidEmail, validNumber, setValidNumber }}
     >
       {children}
     </GlobalContex.Provider>
