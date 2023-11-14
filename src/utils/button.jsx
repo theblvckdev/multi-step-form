cd43d74d60a8af164be7bbf5cc4d36969fef29e5
joeCodes05 <nathancodes05@gmail.com>
2023-11-14 16:06:31 +0100

    code update

--- src/utils/button.jsx
+++ src/utils/button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ text, className, onClick }) => {
   return (
     <>
-      <button onClick={onClick} className={`py-3 px-5 rounded-lg duration-700 ${className}`}>
+      <button onClick={onClick} className={`py-3 outline-none px-5 rounded-lg duration-700 ${className}`}>
         {text}
       </button>
     </>
