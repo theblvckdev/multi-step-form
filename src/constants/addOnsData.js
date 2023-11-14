70b732dac949e94a2ae03a4e3f8b67e1dce253e1
joeCodes05 <nathancodes05@gmail.com>
2023-11-14 23:46:53 +0100

    code update

---
+++ src/constants/addOnsData.js
@@ -0,0 +1,25 @@
+export const addOnsData = [
+    {
+        id: 1,
+        title: 'Online service',
+        feature: 'Access to multiplayer games',
+        monthlyFee: 1,
+        yearlyFee: 10
+    },
+
+    {
+        id: 2,
+        title: 'Larger Storage',
+        feature: 'Extra 1TB of cloud save',
+        monthlyFee: 2,
+        yearlyFee: 20
+    },
+
+    {
+        id: 3,
+        title: 'Customizable profile',
+        feature: 'Custom theme on your profile',
+        monthlyFee: 2,
+        yearlyFee: 20
+    }
+]
